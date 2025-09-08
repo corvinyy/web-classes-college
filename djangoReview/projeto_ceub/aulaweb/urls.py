f5102df99053708a6a2c20c74bb2954903d4de49
Lorena Araujo da Silva <lorena.asilva72@dinfor.uniceub>
2025-09-08 10:45:48 -0300

    feat: criação do produto e tabela que mostra detalhes

--- djangoReview/projeto_ceub/aulaweb/urls.py
+++ djangoReview/projeto_ceub/aulaweb/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sobre', views.sobre, name='sobre')
+    path('sobre', views.sobre, name='sobre'),
+    path('produtos', views.list_produtos, name='produtos'),
+    path('mostra_detalhes/<int:id>', views.mostra_detalhes, name='mostra_detalhes')
 ]
--- djangoReview/projeto_ceub/aulaweb/views.py
+++ djangoReview/projeto_ceub/aulaweb/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def index(request):
-    print('oi')
     return render(request, 'aulaweb/index.html', {})
 
 def sobre(request):
-    print('oi')
     return render(request, 'aulaweb/sobre.html', {'nome_projeto':'Teste'})
+
+def list_produtos(request):
+    i = Produto.objects.all()
+    return render(request, 'aulaweb/produtos.html', {'list_produtos':i})
+
+def infoP(request):
+    return render(request, 'aulaweb/infoP.html', {})
+
+def mostra_detalhes(request, id):
+    produto = None
+    msg = None
+    try: 
+        produto = Produto.objects.get(pk=id)
+    except:
+        msg = 'produto nao encontrado'
+    return render(request, 'aulaweb/infoP.html', {'produto':produto, 'msg': msg})
